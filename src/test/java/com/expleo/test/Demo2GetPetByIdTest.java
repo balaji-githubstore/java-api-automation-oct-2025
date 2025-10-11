53b33b89b596181d3db407a0b6ab499776bbf303
balaji-githubstore <balajidinakaran1@gmail.com>
2025-10-11 12:38:51 +0530

    Added code for add pet

--- src/test/java/com/expleo/test/Demo2GetPetByIdTest.java
+++ src/test/java/com/expleo/test/Demo2GetPetByIdTest.java
@@ -12,43 +12,42 @@ import org.testng.annotations.Test;
  * Deserialization - converting byte stream to proper object
  */
 public class Demo2GetPetByIdTest {
-    public String baseUrl="https://petstore.swagger.io/v2";
+    public String baseUrl = "https://petstore.swagger.io/v2";
 
     /**
      * JSONNode --> Tree like structure - felxible structure - avoid if planning schema validation
      * (Not type safety,no auto mapping nested objects, not easy to maintain, not suitable for api automation)
      */
     @Test
-    public void demo1GetPetByIdJsonNode()
-    {
-        int petId=5;
-        String resource="/pet/"+petId;
+    public void demo1GetPetByIdJsonNode() {
+        int petId = 5;
+        String resource = "/pet/" + petId;
 
-        JsonNode jsonNodeObj= RestAssured
+        JsonNode jsonNodeObj = RestAssured
                 .given()
-                .when().get(baseUrl+resource)
+                .when().get(baseUrl + resource)
                 .then().statusCode(200).extract().as(JsonNode.class);
+
         System.out.println(jsonNodeObj.get("id"));
         System.out.println(jsonNodeObj.get("name").asText());
         System.out.println(jsonNodeObj.get("category").get("id"));
         System.out.println(jsonNodeObj.get("tags").get(0).get("id").asInt());
         System.out.println(jsonNodeObj.get("photoUrls").get(0));
 
-        Assert.assertEquals(jsonNodeObj.get("id").asInt(),5);
+        Assert.assertEquals(jsonNodeObj.get("id").asInt(), 5);
     }
 
     /**
      * only advantage is json expression. (Not type safety,no auto mapping nested objects, not easy to maintain, not suitable for api automation)
      */
     @Test
-    public void demo2GetPetByIdJsonPath()
-    {
-        int petId=5;
-        String resource="/pet/"+petId;
+    public void demo2GetPetByIdJsonPath() {
+        int petId = 5;
+        String resource = "/pet/" + petId;
 
-        JsonPath jsonPathObj= RestAssured
+        JsonPath jsonPathObj = RestAssured
                 .given()
-                .when().get(baseUrl+resource)
+                .when().get(baseUrl + resource)
                 .then().statusCode(200).extract().jsonPath();
 
         System.out.println(jsonPathObj.prettify());
@@ -62,12 +61,11 @@ public class Demo2GetPetByIdTest {
      * Pojo class - type safety, easy maintenance, stable for automation, schema validation
      */
     @Test
-    public void demo3GetPetByIdPojoclass()
-    {
-       Pet petObj= RestAssured
+    public void demo3GetPetByIdPojoclass() {
+        Pet petObj = RestAssured
                 .given()
-                .pathParam("petId",5)
-                .when().get(baseUrl+"/pet/{petId}")
+                .pathParam("petId", 5)
+                .when().get(baseUrl + "/pet/{petId}")
                 .then().statusCode(200).extract().as(Pet.class);
 
         System.out.println(petObj.getId());
@@ -76,18 +74,17 @@ public class Demo2GetPetByIdTest {
     }
 
     @Test
-    public void demo4OpenAPISpecVerify()
-    {
+    public void demo4OpenAPISpecVerify() {
         //option 1 - pass yaml file directly
-        OpenApiValidationFilter validationFilter=new OpenApiValidationFilter("src/test/resources/petstore.yaml");
+        OpenApiValidationFilter validationFilter = new OpenApiValidationFilter("src/test/resources/petstore.yaml");
         //option 2 - pass file object
         //option 3 - pass url
 
-        Pet petObj= RestAssured
+        Pet petObj = RestAssured
                 .given()
                 .filter(validationFilter)
-                .pathParam("petId",50)
-                .when().get(baseUrl+"/pet/{petId}")
+                .pathParam("petId", 50)
+                .when().get(baseUrl + "/pet/{petId}")
                 .then().statusCode(200).extract().as(Pet.class);
 
         System.out.println(petObj.getId());
@@ -95,4 +92,52 @@ public class Demo2GetPetByIdTest {
         System.out.println(petObj.getTags().get(0).getId());
     }
 
+    @Test
+    public void demo5FindInvalidPetTest() {
+        OpenApiValidationFilter validationFilter = new OpenApiValidationFilter("src/test/resources/petstore.yaml");
+
+        RestAssured
+                .given()
+                .filter(validationFilter)
+                .pathParam("petId", 50787887)
+                .when().get(baseUrl + "/pet/{petId}")
+                .then().statusCode(404);
+    }
+
+    @Test
+    public void demo6AddValidPet()
+    {
+        String jsonBody="{\n" +
+                "  \"id\": 556,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"doggie\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+        Pet petResponse= RestAssured
+                .given()
+                .contentType("application/json")
+                .body(jsonBody)
+                .when()
+                .post(baseUrl+"/pet")
+                .then()
+                .statusCode(200).extract().as(Pet.class);
+
+        System.out.println(petResponse.getId());
+
+        //verify @Test is pass or faile
+        Assert.assertEquals(petResponse.getId(),556);
+    }
 }
