96f90c1c9a06f08a0019f0d728d5bfb4d420b221
balaji-githubstore <balajidinakaran1@gmail.com>
2025-10-18 21:37:14 +0530

    updated code

--- src/test/java/com/expleo/test/Demo4GitAPI.java
+++ src/test/java/com/expleo/test/Demo4GitAPI.java
@@ -18,7 +18,7 @@ public class Demo4GitAPI {
         String token=jsonPath.get("token");
 
         OpenApiValidationFilter validationFilter
-                =new OpenApiValidationFilter("src/test/resources/demo.yaml");
+                =new OpenApiValidationFilter("src/test/resources/gitrepo.yaml");
 
         RestAssured.baseURI="https://api.github.com";
         RestAssured
@@ -28,6 +28,4 @@ public class Demo4GitAPI {
                 .when().get("/user/repos")
                 .then().statusCode(200).log().all();
     }
-
-
 }
